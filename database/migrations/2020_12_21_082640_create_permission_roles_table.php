4e0c98d01ed8089eaac0c82cf660ed229a5f3784
Juliemar Añonuevo <mr.juliemar.anonuevo@gmail.com>
2020-12-21 16:39:06 +0800

    Initial Commit

---
+++ database/migrations/2020_12_21_082640_create_permission_roles_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePermissionRolesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('permission_roles', function (Blueprint $table) {
+            $table->string('access_level')->nullable()->default(NULL);
+            $table->unsignedInteger('permission_id')->nullable()->default(NULL);
+            $table->unsignedInteger('role_id')->nullable()->default(NULL);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('permission_roles');
+    }
+}

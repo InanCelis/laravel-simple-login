badd826e0c1164d6458af31762acd2a5dd5b38ec
Juliemar Añonuevo <mr.juliemar.anonuevo@gmail.com>
2021-01-04 21:54:14 +0800

    Added API Passport Oauth

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Models\User;
+use Carbon\Carbon;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth:api',['except'=>'login']);
+    }
+    
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string'
+        ]);
+           
+        $credentials = request(['email', 'password']);
+
+        if (!Auth::attempt($credentials)) {
+            return response()->json([
+                'message' => 'Unauthorized'
+            ], 401);
+        }
+        
+        $user = $request->user();
+
+		$tokenResult = $user->createToken('Personal Access Token');
+
+        $token = $tokenResult->token;
+        
+        $token->save();
+
+        return response()->json([
+            'accessToken' => $tokenResult->accessToken,
+            'tokenType' => 'Bearer',
+            'expiresAt' => Carbon::parse(
+                $tokenResult->token->expires_at
+            )->toIso8601String()
+        ]);
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->token()->revoke();
+
+        return response()->json([
+            'message' => 'Successfully logged out'
+        ]);
+	}
+}
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['prefix'=>'v1'],function(){ 
+    Route::post('/login', [App\Http\Controllers\API\AuthController::class, 'login']);
+    Route::post('/logout', [App\Http\Controllers\API\AuthController::class, 'logout']);
+});
